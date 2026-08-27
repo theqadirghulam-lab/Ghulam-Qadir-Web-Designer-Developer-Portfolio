1a375e345a403729fbba6a72e566877781315dee
theqadirghulam-lab <theqadirghulam@gmail.com>
2026-08-27 11:18:44 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/Ghulam-Qadir-Web-Designer-Developer-Portfolio/"
 });
